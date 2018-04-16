8e64a85006df2b2c87d9844ac878533619d2761e
Ahri <hung.mighty.nguyen@gmail.com>
2018-04-16 20:23:13 +0700

    Fix back button item disappear in iOS 10

--- Pinterest/Views/CustomNavigationController.swift
+++ Pinterest/Views/CustomNavigationController.swift
@@ -19,7 +19,7 @@ class CustomNavigationController: UINavigationController {
     
     self.delegate = self
     self.navigationBar.barTintColor = UIColor.white
-    self.navigationBar.tintColor = UIColor.white
+//    self.navigationBar.tintColor = UIColor.white
     self.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.black]
     self.navigationBar.isTranslucent = false
   }
@@ -30,7 +30,7 @@ extension CustomNavigationController: UINavigationControllerDelegate {
   
   func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
     let item = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
-    item.tintColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+    item.tintColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
     viewController.navigationItem.backBarButtonItem = item
   }
   
